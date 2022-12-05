350cc18cb386c4f8aad64645a8eaac12b22073df
salma <sy138911@gmail.com>
2022-12-05 13:34:58 +0200

    +quran file %sura datails to read file +verse widget

--- lib/sura_details/sura_details_screen.dart
+++ lib/sura_details/sura_details_screen.dart
@@ -2,13 +2,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:untitled/sura_details/sura_details_args.dart';
+import 'package:untitled/sura_details/verse_widget.dart';
 
-class SuraDetailsScreen extends StatelessWidget {
-  static const String routename='sura-details';
+class SuraDetailsScreen extends StatefulWidget {
+  static const String routename = 'sura-details';
+
+  @override
+  State<SuraDetailsScreen> createState() => _SuraDetailsScreenState();
+}
+class _SuraDetailsScreenState extends State<SuraDetailsScreen>{
+  List<String>verses=[];
   @override
   Widget build(BuildContext context) {
     SuraDetailsArgs args=ModalRoute.of(context)?.settings.arguments as SuraDetailsArgs;
-    readFile(args.index+1);
+if(verses.isEmpty)
+  readFile(args.index+1);
     return Container(
         decoration: BoxDecoration(
         image: DecorationImage(
@@ -20,12 +28,36 @@ class SuraDetailsScreen extends StatelessWidget {
        appBar: AppBar(
          title: Text(args.name),
        ),
+       body: verses.isEmpty?
+           Center(child:CircularProgressIndicator(),)
+       : Card(
+         elevation: 12,
+         margin: EdgeInsets.symmetric(horizontal:24,vertical:64 ),
+         shape: RoundedRectangleBorder(
+           borderRadius: BorderRadius.circular(12)
+         ),
+         child:ListView.separated(itemBuilder: (_,index)
+       {
+         return VerseWidget(verses[index],index+1);
+       },
+         itemCount:verses.length,
+         separatorBuilder: (_,__){
+           return Container(
+             color: Theme.of(context).primaryColor,
+             height: 1,
+               width: double.infinity,
+             margin: EdgeInsets.symmetric(horizontal: 64),
+           );
+         },
+         ),)
      ),
     );
   }
   void readFile(int fileIndex)async{
 String fileContent=await rootBundle.loadString('assets/files/$fileIndex.txt');
-List<String>lines=fileContent.split('\n');
-print(lines.length);
+List<String>lines=fileContent.trim().split('\n');
+setState((){
+  verses=lines;
+});
   }
 }
